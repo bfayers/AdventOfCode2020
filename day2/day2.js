0e90589dabbeb2192a1d402d73e178a9764de23e
bfayers <bfayers@users.noreply.github.com>
2020-12-02 15:30:23 +0000

    use forEach it's cleaner

--- day2/day2.js
+++ day2/day2.js
@@ -6,10 +6,11 @@ var lines = fs.readFileSync('./day2.txt', 'utf8').split('\n');
 
 //Make a counter to store number of valid PWs
 var counter = 0;
-//Loop over the lines
-for ( var i = 0; i<lines.length; i++) {
-    let policy = lines[i].split(":")[0];
-    let password = lines[i].split(": ")[1];
+
+//Use forEach to go over every element.
+function part1(element) {
+    let policy = element.split(":")[0];
+    let password = element.split(": ")[1];
     let policyMin = parseInt(policy.split("-")[0]);
     let policyMax = parseInt(policy.split("-")[1].split(" ")[0]);
     let policyLetter = policy.split("-")[1].split(" ")[1];
@@ -20,7 +21,9 @@ for ( var i = 0; i<lines.length; i++) {
         //It's valid
         counter++;
     }
-
 }
+
+
+lines.forEach(part1);
 
 console.log(counter);
