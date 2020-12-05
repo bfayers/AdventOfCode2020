a7c988a4ad6f31b55190ecb8f138da3ffa28f152
bfayers <bfayers@users.noreply.github.com>
2020-12-05 23:17:42 +0000

    remove all unused code

--- day5/main.go
+++ day5/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	//"io/ioutil"
 	"bufio"
 	"os"
 	"sort"
@@ -27,12 +26,6 @@ func readLines(path string) ([][]string, error) {
 }
 
 func main() {
-
-	/*x := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127}   // Intialized with values
-	w := []int{0, 1, 2, 3, 4, 5, 6, 7}   // Intialized with values*/
-	
-	//y := "FBFBBFFRLR"
-	//z := strings.Split(y, "")
 
 	//Read file lines
 	lines, _ := readLines("day5.txt")
@@ -82,33 +75,5 @@ func main() {
 			break
 		}
 	}
-
-
-/*
-	//Go over the first 7 chars of the boarding pass
-	for _, element := range z[0:7] {
-		
-		if element == "F" {
-			//Split to lower
-			x = x[0:len(x)/2]
-		} else if element == "B" {
-			//Split to higher
-			x = x[len(x)/2:len(x)]
-		}
-	}
-	fmt.Println(x)
-
-	//Go over the last 3 chars of the pass
-	for _, element := range z[7:] {
-		if element == "L" {
-			//Split to lower
-			w = w[0:len(w)/2]
-		} else if element == "R" {
-			//Split to higher
-			w = w[len(w)/2:len(w)]
-		}
-	}
-	fmt.Println(w)
-*/
 
 }
