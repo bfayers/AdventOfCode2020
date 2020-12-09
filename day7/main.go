0353e5afb0ea333e56c912e06962b0a629d585e9
bfayers <bfayers@users.noreply.github.com>
2020-12-09 00:29:22 +0000

    this still doesnt work :/

--- day7/main.go
+++ day7/main.go
@@ -24,6 +24,15 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+func Find(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
 func main() {
 	lines, _ := readLines("day7.txt")
 	//bagsToFind := []string{"shiny gold"}
@@ -37,35 +46,45 @@ func main() {
 			bagsEventuallyContainSG = append(bagsEventuallyContainSG, bag)
 		}
 	}
+
 	/*for _, line := range lines {
 		line := strings.Split(line, "contain")
 		for _, bag := range bagsEventuallyContainSG {
 			if strings.Contains(line[1], bag) {
-				fmt.Println(line)
+				bag := strings.Split(line[0], "bags")[0]
+				bagsEventuallyContainSG = append(bagsEventuallyContainSG, bag)
 			}
 		}
-	}*/
-	fmt.Println(len(bagRecurser(bagsEventuallyContainSG, lines, []string{})))
+	}
+	fmt.Println(len(bagsEventuallyContainSG))*/
+
+	fmt.Println(bagRecurser(bagsEventuallyContainSG, lines, []string{}, 0))
 }
 
-func bagRecurser(bags []string, lines []string, prevBags []string) []string {
-	fmt.Println(len(bags))
+func bagRecurser(bags []string, lines []string, checked []string, count int) int {
 
 	if len(bags) == 0 {
-		return prevBags
+		return count
 	}
 	newBags := []string{}
 	for _, bag := range bags {
+		_, found := Find(checked, bag)
+		if found {
+			//We've already checked this bag
+			continue
+		}
 		for _, line := range lines {
 			line := strings.Split(line, "contain")
 			if strings.Contains(line[1], bag) {
-				bagName := strings.Split(line[0], "bags")[0]
+				bagName := strings.Split(line[0], " bags")[0]
 				newBags = append(newBags, bagName)
+				count++
 				//fmt.Println(newBags)
 			}
 		}
+		checked = append(checked, bag)
 	}
-	return bagRecurser(newBags, lines, bags)
+	return bagRecurser(newBags, lines, checked, count)
 	//return bags
 
 }
